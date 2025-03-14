a29c8a88b70662873955600e4e5e1ab4544caf40
DipanjanKarmakar43 <dipanjankarmakar43@gmail.com>
2025-03-14 20:09:53 +0530

    ejs connected

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const Listing = require('./models/listing.js')
-const ejs = require('ejs')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Listing = require('./models/listing.js');
+const path = require('path');
 
 const MONGO_URL = 'mongodb://localhost:27017/airbnb-clone'
 
@@ -17,22 +17,30 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
   res.send('Hi there!');
 });
 
-app.get('/testListings', async (req, res) => {
-  let sampleListing = new Listing({
-    title: 'My New Villa',
-    description: 'By the mountains',
-    price: 12000,
-    location: 'Matheran,Mumbai',
-    country: 'India'
-  });
-  await sampleListing.save();
-  console.log('Sample was saved!');
-  res.send('Success!');
+app.get('/listings', async (req, res) => {
+  const allListings = await Listing.find({})
+  res.render("./listings/index.ejs", {allListings});
 });
+
+// app.get('/testListings', async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: 'My New Villa',
+//     description: 'By the mountains',
+//     price: 12000,
+//     location: 'Matheran,Mumbai',
+//     country: 'India'
+//   });
+//   await sampleListing.save();
+//   console.log('Sample was saved!');
+//   res.send('Success!');
+// });
 
 app.listen(6969, () => {
   console.log(`Example app listening on port 8080`)
---
+++ init/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const initData = require('./data.js');
+const Listing = require('../models/listing.js');
+
+const MONGO_URL = 'mongodb://localhost:27017/airbnb-clone';
+
+main()
+  .then(() => console.log('Connected to DB'))
+  .catch(err => console.error(err));
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  
+  // Ensure images are strings
+  const formattedData = initData.data.map(item => ({
+    ...item,
+    image: item.image.url // Extract only the URL if your schema expects a string
+  }));
+
+  await Listing.insertMany(formattedData);
+  console.log('Data was initialized');
+};
+
+initDB();
