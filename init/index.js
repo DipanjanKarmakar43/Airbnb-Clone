3221b887e4e3093fd9710a3b30bc0730ae56d9ca
DipanjanKarmakar43 <dipanjankarmakar43@gmail.com>
2025-03-17 14:57:16 +0530

    .

--- init/index.js
+++ init/index.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-const initData = require('./data.js');
-const Listing = require('../models/listing.js');
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
-const MONGO_URL = 'mongodb://localhost:27017/airbnb-clone';
+const MONGO_URL = "mongodb://localhost:27017/airbnb-clone";
 
 main()
-  .then(() => console.log('Connected to DB'))
-  .catch(err => console.error(err));
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.error(err));
 
 async function main() {
   await mongoose.connect(MONGO_URL);
@@ -14,15 +14,15 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  
+
   // Ensure images are strings
-  const formattedData = initData.data.map(item => ({
+  const formattedData = initData.data.map((item) => ({
     ...item,
-    image: item.image.url // Extract only the URL if your schema expects a string
+    image: item.image.url, // Extract only the URL if your schema expects a string
   }));
 
   await Listing.insertMany(formattedData);
-  console.log('Data was initialized');
+  console.log("Data was initialized");
 };
 
 initDB();
