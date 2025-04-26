b61b50006ec3cef3371b9400a07c7960b13a421b
DipanjanKarmakar43 <156559424+DipanjanKarmakar43@users.noreply.github.com>
2025-04-26 10:55:56 +0530

    .

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 const MONGO_URL = "mongodb://localhost:27017/airbnb-clone";
 
@@ -50,11 +51,11 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync(async (req, res, next) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
-});
+}));
 
 //Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
@@ -89,6 +90,10 @@ app.delete("/listings/:id", async (req, res) => {
 //   console.log('Sample was saved!');
 //   res.send('Success!');
 // });
+
+app.use((err, req, res, next) => {
+  res.send('Something went wrong!');
+});
 
 app.listen(6969, () => {
   console.log(`Example app listening on port 6969`);
